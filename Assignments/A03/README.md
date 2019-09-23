66c51b4d12f62597eabb4be1e9c5cbc29497e7e7
rugbyprof <terry.griffin@mwsu.edu>
2019-09-23 14:55:35 -0500

    program 3

--- Assignments/A03/README.md
+++ Assignments/A03/README.md
@@ -3,7 +3,7 @@
 
 ### Overview
 
-Using a binary heap as your data structure | read the [data file](./input_data.csv) and print out the order in which they would be processed. The order is based on the priority value. The larger the number | the higher the priority.
+Using a binary heap as your data structure | read a [data file](./animals.json) and print out the order in which they would be processed. The order is based on the priority value. The larger the number | the higher the priority.
 
 | animal_name           | longitude   | latitude    | date        | version | validated | adjustor |
 | :-------------------- | :---------- | :---------- | :---------- | :------ | :-------- | :------- |
@@ -12,12 +12,77 @@ Using a binary heap as your data structure | read the [data file](./input_data.c
 | Chipmunk least        | 39.3988465  | 51.1373971  | -135949885  | 0.3.6   | true      | 0.68     |
 | Common grenadier      | -8.5944594  | 40.557917   | -370927360  | 9.1.7   | true      | 0.75     |
 
+#### Priority
+
 The ***priority*** will be calculated by doing the following:
 
 1. `L` = length of the `animal_name` 
 2. `D` = distance (`latitude`,`longitude`) is from (`33.9137`,`-98.4934`)
 3. `A` = adjustor
+4. `V` = validated
 
-`Priority` = `D*A/L`
 
-Still not done....
+`Bucket` = abs(date) % number_of_buckets
+
+`Priority = (6372.8 - D) * A / L`
+
+if `V` is `true`: invert `Priority`
+
+#### Heap
+
+- You will create `buckets` number of max heaps where order is based on the calculated priority as discussed above. 
+- Assume a random number of `buckets` somewhere between `3-11` (inclusive) to place your animals in. 
+- An animals `bucket` is chosen based on the date field: `abs(date) % number_of_buckets`. 
+
+#### Output
+
+- Perform a `remove_max` on each heap in succession: from heap 0 => N and print the Animals Name + Priority to an output file.
+- Remove the top 5 animals from each heap.
+
+Example:
+```
+Heap 0
+==================
+Springbuck 275.913
+Toddy cat 253.893
+        .
+        .
+        .
+
+Heap 1
+==================
+Black-faced kangaroo -24.45
+Meerkat cat -23.242
+        .
+        .
+        .
+
+Heap N
+==================
+Green heron 345.783
+Blue peacock 334.102
+        .
+        .
+        .
+```
+
+#### Deliverables
+
+- Create a folder called `assignments`
+- In `assignments` create a folder called `A03`
+- In `A03` place your code in a file called `priority_heap.cpp`
+- Name your input file `animals.json` and place that in the `A03` folder.
+- A copy of your generated output.
+- Name printed digitally on all items turned in.
+- Comments:
+
+```
+/**
+ * Name: Your Name
+ * Date: The Date
+ * Course: CMPS 3013
+ * Program: A03
+ * Description:
+ *
+ *      A very good description of your program and any instructions on how to run it.  
+ */
--- Assignments/A03/starter.cpp
+++ Assignments/A03/starter.cpp
@@ -1,58 +1,50 @@
 //https://stackoverflow.com/questions/55116344/how-to-setup-vs-code-for-c-14-c-17
 //https://github.com/nlohmann/json
 
+#include "json_helper.cpp"
 #include <fstream>
 #include <iostream>
-#include "json_helper.cpp"
 
 #define _USE_MATH_DEFINES
- 
+
 #include <math.h>
 
-
 using namespace std;
- 
+
 const static double EarthRadiusKm = 6372.8;
- 
-inline double DegreeToRadian(double angle)
-{
-	return M_PI * angle / 180.0;
+
+inline double DegreeToRadian(double angle) {
+    return M_PI * angle / 180.0;
 }
- 
-class Coordinate
-{
+
+class Coordinate {
 public:
-	Coordinate(double latitude ,double longitude):myLatitude(latitude), myLongitude(longitude)
-	{}
- 
-	double Latitude() const
-	{
-		return myLatitude;
-	}
- 
-	double Longitude() const
-	{
-		return myLongitude;
-	}
- 
+    Coordinate(double latitude, double longitude) : myLatitude(latitude), myLongitude(longitude) {}
+
+    double Latitude() const {
+        return myLatitude;
+    }
+
+    double Longitude() const {
+        return myLongitude;
+    }
+
 private:
- 
-	double myLatitude;
-	double myLongitude;
+    double myLatitude;
+    double myLongitude;
 };
- 
-double HaversineDistance(const Coordinate& p1, const Coordinate& p2)
-{
-	double latRad1 = DegreeToRadian(p1.Latitude());
-	double latRad2 = DegreeToRadian(p2.Latitude());
-	double lonRad1 = DegreeToRadian(p1.Longitude());
-	double lonRad2 = DegreeToRadian(p2.Longitude());
- 
-	double diffLa = latRad2 - latRad1;
-	double doffLo = lonRad2 - lonRad1;
- 
-	double computation = asin(sqrt(sin(diffLa / 2) * sin(diffLa / 2) + cos(latRad1) * cos(latRad2) * sin(doffLo / 2) * sin(doffLo / 2)));
-	return 2 * EarthRadiusKm * computation;
+
+double HaversineDistance(const Coordinate &p1, const Coordinate &p2) {
+    double latRad1 = DegreeToRadian(p1.Latitude());
+    double latRad2 = DegreeToRadian(p2.Latitude());
+    double lonRad1 = DegreeToRadian(p1.Longitude());
+    double lonRad2 = DegreeToRadian(p2.Longitude());
+
+    double diffLa = latRad2 - latRad1;
+    double doffLo = lonRad2 - lonRad1;
+
+    double computation = asin(sqrt(sin(diffLa / 2) * sin(diffLa / 2) + cos(latRad1) * cos(latRad2) * sin(doffLo / 2) * sin(doffLo / 2)));
+    return 2 * EarthRadiusKm * computation;
 }
 
 struct Animal {
@@ -64,7 +56,7 @@ struct Animal {
     bool validated;
     string version;
 
-    Animal(){
+    Animal() {
         animal_name = "";
         date = 0;
         latitude = 0.0;
@@ -95,9 +87,7 @@ struct Animal {
     }
 };
 
-
-
-class AnimalHelper{
+class AnimalHelper {
 private:
     Animal **Animals;
     JsonHelper *J;
@@ -105,34 +95,37 @@ private:
     int size;
 
 public:
-    AnimalHelper(string filename){
+    AnimalHelper(string filename) {
         J = new JsonHelper(filename);
         size = J->getSize();
-        Animals = new Animal*[size];
-        for(int i=0;i<size;i++){
+        Animals = new Animal *[size];
+        for (int i = 0; i < size; i++) {
             obj = J->getNext();
             Animals[i] = new Animal(obj);
         }
     }
 
-    void PrintAnimals(){
+    void PrintAnimals() {
 
-        for(int i=0;i<size;i++){
-            cout<<Animals[i]->animal_name<<" "<<
-                Animals[i]->latitude<<" "<<
-                Animals[i]->longitude<<" "<<
-                Animals[i]->date<<" "<<
-                Animals[i]->priority<<" "<<
-                Animals[i]->validated<<" "<<
-                Animals[i]->version<<endl;
+        for (int i = 0; i < size; i++) {
+
+            cout << Animals[i]->animal_name << " " 
+            << Animals[i]->latitude << " " 
+            << Animals[i]->longitude << " " 
+            << Animals[i]->date << " " 
+            << Animals[i]->priority << " " 
+            << Animals[i]->validated << " " 
+            << Animals[i]->version << endl;
+
         }
     }
-
 };
+
+
 
 int main(int argc, char **argv) {
 
-    AnimalHelper AH("input_file.json");
+    AnimalHelper AH("animals.json");
 
     AH.PrintAnimals();
 
