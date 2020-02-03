9c80308309d62bb311edf9a2250b08fca166588e
rugbyprof <terry.griffin@mwsu.edu>
2020-02-03 16:53:28 -0600

    changes

--- Resources/06-Getch/main.cpp
+++ Resources/06-Getch/main.cpp
@@ -4,10 +4,22 @@
 using namespace std;
 
 int main() {
-    char k;
+    char k;             // holder for character being typed
+    string word = "";   // var to concatenate letters to
 
+    // While capital Z is not typed keep looping
     while ((k = getch()) != 'Z') {
-        cout << "Key: " << k << " = " << (int)k << endl;
+        word += k;          // append char to word
+
+        if((int)k != 10){   // if k is not a space print it
+            cout << "Key: " << k << " = " << (int)k << endl;
+            cout << "Word: " << word << endl;
+        }
+
+        // hitting enter sets word back to empty
+        if((int)k == 10 ){
+            word = "";
+        }
     }
     return 0;
 }
