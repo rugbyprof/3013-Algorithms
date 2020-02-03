e5c01c312c0de05cbf0d43b56508d57f6ce37e96
rugbyprof <terry.griffin@mwsu.edu>
2020-02-03 16:47:02 -0600

    fix

--- Resources/06-Getch/main.cpp
+++ Resources/06-Getch/main.cpp
@@ -1,12 +1,6 @@
 #include "mygetch.hpp"
 #include <iostream>
 
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
-#define OS "WINDOWS"
-#else
-#define OS "LINUX"
-#endif
-
 using namespace std;
 
 int main() {
