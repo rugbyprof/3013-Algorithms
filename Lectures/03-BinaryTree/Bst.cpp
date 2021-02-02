b40c56f3c6bd9c243e91a0ac4afc18cfd0d2dce6
rugbyprof <terry.griffin@msutexas.edu>
2021-02-01 23:53:15 -0600

    cleaner

--- Lectures/03-BinaryTree/Bst.cpp
+++ Lectures/03-BinaryTree/Bst.cpp
@@ -3,147 +3,130 @@
 */
 #include "Bst.h"
 
-
-Node::Node(){
+Node::Node() {
     data = 0;
     left = right = NULL;
 }
 
-Node::Node(int x){
+Node::Node(int x) {
     data = x;
     left = right = NULL;
 }
 
-
-Bst::Bst(){
+Bst::Bst() {
     size = 0;
     root = NULL;
 }
 
-Bst::Bst(int *A){
-
+Bst::Bst(int *A) {
 }
 
-void Bst::Insert(Node* &root, int val){
-    if(!root){
+void Bst::Insert(Node *&root, int val) {
+    if (!root) {
         root = new Node(val);
         return;
-    }else{
-        if(root->data > val){
-            Insert(root->left,val);
-        }else{
-            Insert(root->right,val);
+    } else {
+        if (root->data > val) {
+            Insert(root->left, val);
+        } else {
+            Insert(root->right, val);
         }
     }
 }
 
-void Bst::Insert(int data){
+void Bst::Insert(int data) {
 
-    if(!root){
-        Node* temp = new Node(data);
+    if (!root) {
+        Node *temp = new Node(data);
         root = temp;
-    }else{
-        Insert(root,data);
+    } else {
+        Insert(root, data);
     }
 }
 
-void Bst::Print(){
+void Bst::Print() {
     Print(root);
 }
 
-void Bst::Print(Node* root){
-    if(!root){
+void Bst::Print(Node *root) {
+    if (!root) {
         return;
-    }else{
+    } else {
         //PreOrder
         Print(root->left);
-        cout<<root->data<<" ";
+        cout << root->data << " ";
         //InOrder
         Print(root->right);
         //PostOrder
     }
 }
 
-void Bst::BuildGraphViz(Node* root){
-    if(!root){
+void Bst::BuildGraphViz(Node *root) {
+    if (!root) {
         return;
-    }else{
+    } else {
         BuildGraphViz(root->left);
 
-        if(!G.nodeExists(to_string(root->data))){
+        if (!G.nodeExists(to_string(root->data))) {
             G.addNode(to_string(root->data));
         }
-        
-        if(root->left){
-            string id = to_string(root->data)+"."+to_string(root->left->data);
-            if(!G.edgeExists(id)){
-                G.addEdge(to_string(root->data),to_string(root->left->data));
-                G.addEdgeStyle(id,"color","blue");
-                G.addEdgeStyle(id,"label","-");
+
+        if (root->left) {
+            string id = to_string(root->data) + "." + to_string(root->left->data);
+            if (!G.edgeExists(id)) {
+                G.addEdge(to_string(root->data), to_string(root->left->data));
+                G.addEdgeStyle(id, "color", "blue");
+                G.addEdgeStyle(id, "label", "-");
             }
 
-        }else{
-            string toid = "NULL"+to_string(rand());
-            string id = to_string(root->data)+"."+toid;
-            G.addEdge(to_string(root->data),toid);
-            G.addNodeStyle(toid,"shape","point"); 
-            G.addNodeStyle(toid,"label","NULL"); 
-            G.addNodeStyle(to_string(root->data),"style","filled"); 
-            G.addNodeStyle(to_string(root->data),"fontcolor","white"); 
-            G.addEdgeStyle(id,"minlen","1"); 
-        }
-        if(root->right){
-            string id = to_string(root->data)+"."+to_string(root->right->data);
-            if(!G.edgeExists(id)){
-                G.addEdge(to_string(root->data),to_string(root->right->data));
-                G.addEdgeStyle(id,"color","red");
-                G.addEdgeStyle(id,"label","+");
+        } else if (root->right) {
+            string id = to_string(root->data) + "." + to_string(root->right->data);
+            if (!G.edgeExists(id)) {
+                G.addEdge(to_string(root->data), to_string(root->right->data));
+                G.addEdgeStyle(id, "color", "red");
+                G.addEdgeStyle(id, "label", "+");
             }
-        }else{
-            string toid = "NULL"+to_string(rand());
-            string id = to_string(root->data)+"."+toid;
-            G.addEdge(to_string(root->data),toid);
-            G.addNodeStyle(toid,"shape","point"); 
-            G.addNodeStyle(toid,"label","NULL"); 
-            G.addNodeStyle(to_string(root->data),"style","filled"); 
-            G.addNodeStyle(to_string(root->data),"fontcolor","white"); 
-            G.addEdgeStyle(id,"minlen","1");
-            
+        } else {
+            string toid = "NULL" + to_string(rand());
+            string id = to_string(root->data) + "." + toid;
+            G.addEdge(to_string(root->data), toid);
+            G.addNodeStyle(toid, "shape", "point");
+            G.addNodeStyle(toid, "label", "NULL");
+            G.addNodeStyle(to_string(root->data), "style", "filled");
+            G.addNodeStyle(to_string(root->data), "fontcolor", "white");
+            G.addEdgeStyle(id, "minlen", "1");
         }
         BuildGraphViz(root->right);
     }
 }
 
-void Bst::BuildGraphViz(){
+void Bst::BuildGraphViz() {
     BuildGraphViz(root);
 }
 
-
-void Bst::PrintGraphViz(){
-    cout<<G.dumpGraph()<<endl;
+void Bst::PrintGraphViz() {
+    cout << G.dumpGraph() << endl;
 }
 
-bool Bst::Find(Node* root,int key){
-    if(!root){
+bool Bst::Find(Node *root, int key) {
+    if (!root) {
         return false;
-    }else{
+    } else {
         //PreOrder
-        if(root->data == key){
+        if (root->data == key) {
             return true;
-        }else if(key < root->data){
-            return Find(root->left,key);
-        }else{
-            return Find(root->right,key);
+        } else if (key < root->data) {
+            return Find(root->left, key);
+        } else {
+            return Find(root->right, key);
         }
     }
-   
 }
 
-bool Bst::Find(int key){
-    
-    return Find(root,key);
+bool Bst::Find(int key) {
+
+    return Find(root, key);
 }
 
-void Bst::Delete(){
-
+void Bst::Delete() {
 }
