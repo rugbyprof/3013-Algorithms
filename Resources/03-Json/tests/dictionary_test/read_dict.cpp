1414a03c6597fbaabc5010483829423d76c42f29
rugbyprof <terry.griffin@msutexas.edu>
2021-02-22 23:00:58 -0600

    comments

--- Resources/03-Json/tests/dictionary_test/read_dict.cpp
+++ Resources/03-Json/tests/dictionary_test/read_dict.cpp
@@ -1,35 +1,38 @@
 /**
- * Make sure before running, you copy necessary .hpp files
- * into your project folder so they can be found.
- * 
- * Trying to run this code as is will fail since the .hpp
- * files our in the resources folder. 
- * 
+ * This file reads in a json array, times it, and prints it out. 
  */
 
 #include <iostream>
-#include "JsonFacade.hpp"       // need to grab a copy from resources folder
+#include "JsonFacade.hpp"
 #include <time.h>
 #include <chrono> 
-#include "Timer.hpp"            // need to grab a copy from resources folder
+#include "Timer.hpp"      
 
 using namespace std;
 
 int main(){
     Timer T;
-    T.Start();
+    T.Start();                   // Start timer
     JsonFacade J("dict.json");   // create instance of json class
-    T.End(); 
-    double s = T.Seconds();
-    long m = T.MilliSeconds();
+    T.End();                     // End Timer
+    double s = T.Seconds();      // get seconds from timer
+    long m = T.MilliSeconds();   // get milliseconds
 
+    cout <<s << " seconds to read in json" <<endl;
+    cout <<m << " milli to read in json" <<endl;
+
+    // loop through json array 
     for(int i=0;i<J.getSize();i++){
+        // everything is a "json" object that you pull out of JsonFacade
         json word = J.getIth(i);
+
+        // so we convert it to a real string to print it out
         std::cout<< word.get<std::string>()<<std::endl;
     }
 
+    // call timer end again to get another length of run time.
     T.End(); 
-    cout <<s << " seconds" <<endl;
-    cout <<m << " milli" <<endl;
+    cout <<T.Seconds() << " seconds to read in and print json" <<endl;
+    cout <<T.MilliSeconds() << " milli to read in and print json" <<endl;
 
 }
