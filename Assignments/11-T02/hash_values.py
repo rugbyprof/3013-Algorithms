0c11f50ca91a5368c69ba01aaefa7c30774fbbf4
rugbyprof <terry.griffin@msutexas.edu>
2021-04-13 04:40:18 -0500

    Pushed from Chickenbutt-15

--- Assignments/11-T02/hash_values.py
+++ Assignments/11-T02/hash_values.py
@@ -1,5 +1,11 @@
 #!/usr/local/bin/python3
 
+from rich.console import Console
+from rich.table import Table
+from rich import print
+from rich.layout import Layout
+from rich.live import Live
+from time import sleep
 
 class HashTable:
     """Simple implementation that hashes integers only
@@ -7,21 +13,40 @@ class HashTable:
     def __init__(self,table_size,collision_method):
         self.size = table_size
         self.collision_method = collision_method
-        self.table = [-1 for x in range(self.size)]
+        self.htable = [-1 for x in range(self.size)]
+
+        self.console = Console()
+
+        self.layout = Layout()
+
+        self.layout.split_row(
+            Layout(name="left"),
+            Layout(name="middle"),
+            Layout(name="right")
+        )
+
+        self.rtable = Table(title=self.collision_method)
+        self.rtable.add_column("#",style="cyan", no_wrap=True)
+        self.rtable.add_column("Value", style="magenta")
+
+        for i in range(self.size):
+            self.rtable.add_row(f"{i}", "")
+        
+        
         
 
     def __str__(self):
         s = ''
         for i in range(self.size):
             if self.collision_method != 'chaining':
-                if self.table[i] > 0:
-                    s += f"{i} {self.table[i]}\n"
+                if self.htable[i] > 0:
+                    s += f"{i} {self.htable[i]}\n"
                 else:
                     s += f"{i} \n"
             else:
-                if isinstance(self.table[i],list):
-                    s += f"{i} {str(self.table[i])}\n"
-                    # for k in self.table[i]:
+                if isinstance(self.htable[i],list):
+                    s += f"{i} {str(self.htable[i])}\n"
+                    # for k in self.htable[i]:
                     #     s += f"{i} {k}->"
                 else:
                     s += f"{i}\n"
@@ -31,18 +56,18 @@ class HashTable:
     def chaining(self,item):
         key = item % self.size
 
-        if not isinstance(self.table[key],list):
-            self.table[key] = []
+        if not isinstance(self.htable[key],list):
+            self.htable[key] = []
 
-        self.table[key].append(item)
+        self.htable[key].append(item)
 
     def linear_probing(self,item):
         key = item % self.size
 
-        while self.table[key] > 0:
+        while self.htable[key] > 0:
             key = (key + 1) % self.size
 
-        self.table[key] = item
+        self.htable[key] = item
 
     def quadratic_probing(self,item):
         probe_sequence = [item]
@@ -52,12 +77,12 @@ class HashTable:
 
         probe_sequence.append(key)
 
-        while self.table[key] > 0:
+        while self.htable[key] > 0:
             i += 1
             key = ((item % self.size) + i**2) % self.size
             probe_sequence.append(key)
 
-        self.table[key] = item
+        self.htable[key] = item
         print(probe_sequence)
 
 
@@ -71,6 +96,12 @@ class HashTable:
             self.chaining(item)
         else:
             print("Error: collision method unkown!")
+
+    def print_layout(self):
+        self.layout["left"].update(self.rtable)
+        return self.layout
+
+        
     
 
 
@@ -79,21 +110,24 @@ if __name__=='__main__':
     nums = [8, 2, 7, 18, 15, 19, 23, 15, 20, 16]
     H1 = HashTable(13,'linear_probing')
 
-    for n in nums:
-        H1.insert(n)
-
-    print(H1)
+    #print(H1)
 
     H2 = HashTable(13,'quadratic_probing')
 
     for n in nums:
         H2.insert(n)
 
-    print(H2)
+    #print(H2)
 
     H3 = HashTable(13,'chaining')
 
     for n in nums:
         H3.insert(n)
 
-    print(H3)
+    with Live(H1.print_layout(), refresh_per_second=4) as live:  # update 4 times a second to feel fluid
+        for row in range(12):
+            for n in nums:
+                H1.insert(n)
+            sleep(0.4)
+            
+
